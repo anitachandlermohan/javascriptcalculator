73c58f8795977ad1b7b1ae09ae4360b8b31b4680
Anita Mohan <anitachandlermohan@gmail.com>
2019-01-15 17:36:49 +0200

    initial commit

---
+++ calculator.js
@@ -0,0 +1,40 @@
+function add(){
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    
+    let num3 = Number(num1)+Number(num2);
+    result.value = num3
+    return num3;
+
+}
+console.log(add());
+
+function subtract(){
+    return num3 - num4;
+
+
+}
+ 
+function divide(){
+    return num5/num6;
+
+
+}
+function multiply(){
+    return num7 * num8;
+
+}
+
+function square(){
+    return num9*num9;
+
+}
+ 
+function squareRoot(){
+    return sqrt(num10);
+}
+
+function factorial(){
+    return 
+
+}
